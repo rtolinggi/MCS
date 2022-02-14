2e248d683f49d4b502220f966cc621a5af4746da
rtolinggi <rtolinggi91@gmail.com>
2022-02-14 14:13:38 +0800

    Register API

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsers } from "../controllers/UsersControllers.js";
+import { getUsers, Register } from "../controllers/UsersControllers.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
+router.post("/users", Register);
 
 export default router;
